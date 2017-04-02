5ec7ea07e14d4ea34cb945ed82a1343f6038729f
hoferj7 <hoferj7@mymacewan.ca>
2017-04-02 16:44:22 -0600

    Fix map view issue

--- django/explore/urls.py
+++ django/explore/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^$', views.search, name='search'),
     url(r'^view/', views.results, name='view'),
     url(r'^weight/', views.weight, name='weight'),
-    url(r'^bargraphs/', views.bargraphs, name='bargraphs')
-    url(r'^details/', views.details, name = 'details'),
+    url(r'^bargraphs/', views.bargraphs, name='bargraphs'),
+    url(r'^details/', views.details, name = 'details')
 ]
